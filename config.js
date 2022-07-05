f1e031877462b022fb9e9f7f62b5f3d98cb761b3
ail122 <107312683+ail122@users.noreply.github.com>
2022-07-05 15:19:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6289516947204'],
+  ['6288213988636'],
+  ['6289516947204', 'BG ONWER', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/6289516947204'
+global.author = 'Iyan-Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
